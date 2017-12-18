80bbe999701bc6c58d273bec0469211a906be38a
Imottall <tjbuiten@avans.nl>
2017-12-18 17:05:46 +0100

    Last commit updated

---
+++ src/app/forums/forums-start/forums-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forums-start',
+  templateUrl: './forums-start.component.html',
+  styleUrls: ['./forums-start.component.css']
+})
+export class ForumsStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
